ac03118bab6e3cd682bae341415f1ab86bccb6bb
Cid Vinícius <cidvfbrandao@gmail.com>
2024-06-20 21:36:17 -0300

    coisas
    muitas mudanças feitas e muitas coisas pra aprender junto com isso, nós vamos apanhar pra conseguir fazer mas vamos com tudo que vai dar certo (eu espero) Boa sorte jão

---
+++ express-update/src/controller/pages.js
@@ -0,0 +1,22 @@
+
+async function retornaSimplificado(req, res, arquivo, dados) {
+    return res.render(arquivo, { ...dados });
+}
+
+async function listaDeDesejos(req, res) {
+    return retornaSimplificado(req, res, 'listaDeDesejos',{ nomes :['oi','aaa','aaa','aaa','aaa','aaa','aaa','aaa','aaa','aaa','aaa','aaa','aaa','aaa','aaa','aaa','aaa','aaa','aaa','aaa']})
+}
+
+async function menu(req, res) {
+    return retornaSimplificado(req, res, 'menu', {})
+}
+
+async function cadastro(req, res) {
+    return retornaSimplificado(req, res, 'cadastro', {})
+}
+
+async function login(req, res) {
+    return retornaSimplificado(req, res, 'login', {})
+}
+
+export { retornaSimplificado, listaDeDesejos , menu,login,cadastro }
--- express-update/src/routes/index.js
+++ express-update/src/routes/index.js
@@ -1,15 +1,19 @@
 import express from "express";
+import { listaDeDesejos, menu , login , cadastro } from "../controller/pages.js";
 
 const router = express.Router()
 
 // So que se liga se tu nào tiver uma rota so com o "/" quando tu conectar so em localhost:3000 vai dar erro
 // Por isso a "/" geralmente é a rota que se entra no site tipo a pagina principal dele
 
+
 //ss isso era mais pra no caso de por nos outros tipo:
-router.get("/", function (req, res, next) {res.render("menu", { title: "Menu"});});
-router.get("/cadastro", function (req, res, next) {res.render("cadastro", { title: "cadastro"});});
-router.get("/login", function (req, res, next) {res.render("login", { title: "login"});});
-router.get("/listaDeDesejos", function (req, res, next) {res.render("listaDeDesejos", { title: "listaDeDesejos"});});
+router.get("/", menu);
+router.get("/cadastro", cadastro);
+router.get("/login", login);
+router.get("/listaDeDesejos", listaDeDesejos);
+
+
 
 //seguindo
 export default router;
